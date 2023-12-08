2892dc3adce205692e0246f937a50f285252df54
Rishabh-Saket <87156299+Rishabh-Saket@users.noreply.github.com>
2023-12-08 10:59:51 +0530

    Added services and about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assests/me2.jpg'
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { LuFolderKanban } from "react-icons/lu";
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='About Image'/>
+         </div>
+        </div>
+        
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small>Fresher</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about_icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <LuFolderKanban className='about_icon'/>
+              <h5>Projects</h5>
+              <small>15+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Hello! I'm Rishabh Saket, a recent graduate with a passion for software engineering. 
+
+            Graduated in 2022, I'm on a mission to make my mark in the world of technology. 
+
+            Aspiring Software Engineer: Actively seeking a full-time position to contribute my skills and knowledge to an innovative organization. 
+
+            My Toolbox:
+            Languages: Java, JavaScript
+            Technologies: REST API, Microservices, JDBC, SQL, Multi-threading, Socket Programming
+            Problem-Solving: Strong grasp of Data Structures and Algorithms
+            Concepts: DBMS, OS, Computer Networks, OOPs
+            I believe in the power of collaboration and continuous learning. Let's connect and explore how we can make technology work for us.
+          </p>
+          <a href='#contact' className='btn btn-primary'>Let's talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,107 @@
 import React from 'react'
 import './services.css'
+import { MdCheck } from "react-icons/md";
 const Services = () => {
   return (
-    <section id='service'>Services</section>
+    <section id='service'>
+        <h5>What I Offer</h5>
+        <h2>Services</h2>
+
+        <div className='container services__container'>
+          {/* Start of UI */}
+          <article className='service'>
+            <div className='service__head'>
+              <h3>UI/UX Design</h3>
+            </div>
+
+            <ul className='service__list'>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+            </ul>
+          </article>
+        {/** END OF UI */}
+
+        {/* Start of Web Development */}
+        <article className='service'>
+            <div className='service__head'>
+              <h3>Web Development</h3>
+            </div>
+
+            <ul className='service__list'>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+            </ul>
+          </article>
+        {/** END OF Web Development */}
+
+        {/* Start of Web Development */}
+        <article className='service'>
+            <div className='service__head'>
+              <h3>Backend Development</h3>
+            </div>
+
+            <ul className='service__list'>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+              <li>
+                <MdCheck className='service__list-icon' />
+                <p>lksmlskmvsfdlkv sddvl sdv sdvkmsdv sdv sd v sdv</p>
+              </li>
+            </ul>
+          </article>
+        {/** END OF Web Development */}
+        </div>
+    </section>
   )
 }
 
