afe40068273926bbef88eb02ed31117bc599b9af
Rishabh-Saket <87156299+Rishabh-Saket@users.noreply.github.com>
2023-12-29 16:39:46 +0530

    integrated email service

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './Contact.css'
 import { MdOutlineEmail } from "react-icons/md";
-
+import {useRef} from 'react';
+import emailjs from 'emailjs-com'
 const Contact = () => {
+  const form=useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_iexxnui', 'template_62wfei7', form.current, '16Rfy9kkFBMtIoPnZ')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    
+    e.target.reset();
+  };
+
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
       <div className='container contact__container'>
         <div className='contact__options'>
           <article className='contact__option'>
@@ -15,11 +33,11 @@ const Contact = () => {
             <a href='mailto:rsssaket99@gmail.com' target='_blank'> Send a message</a>
           </article>
         </div>
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your full name' required />
           <input type='email' name='email' placeholder='Your Email' required />
           <textarea name='message' rows='7' placeholder='Your message' required></textarea>
-          <button type='button' className='btn btn-primary'>Send</button>
+          <button type='submit' className='btn btn-primary'>Send</button>
         </form>
       </div>
     </section>
