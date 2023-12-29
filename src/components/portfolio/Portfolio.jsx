d0b33739d492e9f74f4ad8d10a252d9f57e3358e
Rishabh-Saket <87156299+Rishabh-Saket@users.noreply.github.com>
2023-12-29 15:17:42 +0530

    Added dynamic components for projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,74 @@
 import React from 'react'
 import './portfolio.css'
+
+const data=[
+  {
+    id: 1,
+    image: "NA",
+    title: 'FlyEase',
+    github: "https://github.com/Rishabh-Saket"
+  },
+  {
+    id: 1,
+    image: "NA",
+    title: 'FlyEase',
+    github: "https://github.com/Rishabh-Saket"
+  },
+  {
+    id: 1,
+    image: "NA",
+    title: 'FlyEase',
+    github: "https://github.com/Rishabh-Saket"
+  },
+  {
+    id: 1,
+    image: "NA",
+    title: 'FlyEase',
+    github: "https://github.com/Rishabh-Saket"
+  },
+  {
+    id: 1,
+    image: "NA",
+    title: 'FlyEase',
+    github: "https://github.com/Rishabh-Saket"
+  },
+  {
+    id: 1,
+    image: "NA",
+    title: 'FlyEase',
+    github: "https://github.com/Rishabh-Saket"
+  },
+  {
+    id: 1,
+    image: "NA",
+    title: 'FlyEase',
+    github: "https://github.com/Rishabh-Saket"
+  }
+];
+
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My recent work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio__container'>
+      {
+        data.map(({id,image,title,github})=> {
+          return(
+            <article key={id} className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={image} alt='title'/>
+            </div>
+            <h3>{title}</h3>
+            <div className='portfolio_item-cta'>
+              <a href={github} className='btn'>Github</a>
+            </div>
+            </article>
+          )          
+        })
+      }        
+      </div>
+    </section>
   )
 }
 
