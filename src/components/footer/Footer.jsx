4a7cef1a76809f8c07325b92d2a55f9b1b3a0c2a
Rishabh-Saket <87156299+Rishabh-Saket@users.noreply.github.com>
2024-01-10 16:29:52 +0530

    Added footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import './footer.css'
+import { IoLogoGithub } from "react-icons/io";
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>SAKET</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experiences'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://github.com/Rishabh-Saket'><IoLogoGithub /></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
